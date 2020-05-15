94ff440badf13b07f45f93856fe474aed01c8cfc
lucreciafdez <lfernandez4@uc.cl>
2020-05-15 14:28:26 -0400

    Updates

---
+++ precache-manifest.bdfc793bbf5b0a0740c6f1c910546980.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4fe9dcaeae79221d7a0dd8f46380a85",
+    "url": "/websocket/index.html"
+  },
+  {
+    "revision": "8a3140690be7968eb357",
+    "url": "/websocket/static/css/2.1d984fbf.chunk.css"
+  },
+  {
+    "revision": "6008e6433ddfd1a145da",
+    "url": "/websocket/static/css/main.5efdb265.chunk.css"
+  },
+  {
+    "revision": "8a3140690be7968eb357",
+    "url": "/websocket/static/js/2.9bfca119.chunk.js"
+  },
+  {
+    "revision": "b1309dea080d55c6e0337ce29019146a",
+    "url": "/websocket/static/js/2.9bfca119.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6008e6433ddfd1a145da",
+    "url": "/websocket/static/js/main.4d72d639.chunk.js"
+  },
+  {
+    "revision": "73f889a9c58283f392fb",
+    "url": "/websocket/static/js/runtime-main.821b3932.js"
+  }
+]);
